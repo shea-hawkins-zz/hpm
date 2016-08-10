c42c3d33f75933a359ff0a4ece44df477d203616
Shea Hawkins <Shea Hawkins>
2016-08-10 07:17:45 -0700

    Resolve line-ending bug

--- src/scripts/buildExec.sh
+++ src/scripts/buildExec.sh
@@ -1,1 +1,1 @@
-cat execWrapper.sh ../bazel-out/bapm_deploy.jar > ../built/bapm && cd .. && chmod +x built/bapm
+cat execWrapper.sh ../bazel-out/bapm_deploy.jar > ../built/bapm && chmod +x ../built/bapm
