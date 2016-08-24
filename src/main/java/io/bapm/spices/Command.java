67e142c1ddf3ebe47c43f1eb321fe62aef31677e
Shea Hawkins <Shea Hawkins>
2016-08-24 06:51:37 -0700

    Add command execution to Spice

--- src/main/java/io/bapm/spices/Command.java
+++ src/main/java/io/bapm/spices/Command.java
@@ -1,7 +1,12 @@
 package io.bapm;
 
 import java.util.ArrayList;
+import com.beust.jcommander.JCommander;
 
 public abstract class Command {
   public ArrayList<String> names = new ArrayList<String>();
+  private final Action action;
+  public void execute(JCommander jc) {
+    action.execute(jc);
+  }
 }
--- src/main/java/io/bapm/spices/Spice.java
+++ src/main/java/io/bapm/spices/Spice.java
@@ -1,6 +1,7 @@
 package io.bapm;
 
 import java.util.ArrayList;
+import com.beust.jcommander.JCommander;
 
 public abstract class Spice {
   private ArrayList<Command> commands = new ArrayList<Command>();
@@ -12,5 +13,14 @@ public abstract class Spice {
   }
   public void addCommand(Command command) {
     commands.add(command);
+  }
+  public execute(JCommander jc) {
+    String targetCommand = jc.getParsedCommand();
+    commands.forEach(command -> {
+      command.names.forEach(name -> {
+        if (name.equals(targetCommand)) {
+          command.execute(jc);
+        }
+    });
   }
 }
